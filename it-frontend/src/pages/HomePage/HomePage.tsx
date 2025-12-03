2fd8e703b2f49ca9a739df10eebcb7b06891138c
CserhiBerni <59263479+CserhiBerni@users.noreply.github.com>
2025-12-03 23:28:02 +0100

    Add start of home page design

--- it-frontend/src/pages/HomePage/HomePage.tsx
+++ it-frontend/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,62 @@
 import "./HomePage.css";
+import bali from '../../images/bali.jpg'
+import hawaii from '../../images/hawaii.jpg'
+import maldives from '../../images/maldives.jpg'
 
 export const HomePage = () => {
   return (
     <div className="home-page">
-      <div>HomePage</div> 
+      <section className="hero">
+        <article className="hero-content">
+          <h1 className="hero-title">
+            Discover your <br />
+            next destination
+          </h1>
+          <h2 className="hero-subtitle">
+            Plan trips, explore cities and start your journey with <span className="brand">TravelMe</span>.
+          </h2>
+          <button>Start Exploring</button>
+        </article>
+      </section>
+      <section className="featured">
+        <article className="featured-card">
+          <img src={bali} alt="" />
+          <h3>Bali</h3>
+          <p className="description"></p>
+        </article>
+        <article className="featured-card">
+          <img src={hawaii} alt="" />
+          <h3>Hawaii</h3>
+          <p className="description"></p>
+        </article>
+        <article className="featured-card">
+          <img src={maldives} alt="" />
+          <h3>Maldives</h3>
+          <p className="description"></p>
+        </article>
+      </section>
+      <section className="stats">
+        <article className="stats-card">
+          <i></i>
+          <h3>Explore popular destinations</h3>
+          <p className="description"></p>
+        </article>
+        <article className="stats-card">
+          <i></i>
+          <h3>Explore popular destinations</h3>
+          <p className="description"></p>
+        </article>
+        <article className="stats-card">
+          <i></i>
+          <h3>Explore popular destinations</h3>
+          <p className="description"></p>
+        </article>
+      </section>
+      <section className="planner"></section>
+      <section className="account">
+        <h3>Ready for your next adventure?</h3>
+        <button>Create Account</button>
+      </section>
     </div>
   );
 };
